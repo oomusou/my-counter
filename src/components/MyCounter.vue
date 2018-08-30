65b97e578af94d70fa8ae98ab33a285b11162a8f
oomusou <oomusou@gmail.com>
2018-08-30 17:12:55 +0800

    MyCounter

---
+++ src/components/MyCounter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ innerCounter }}</h1>
+    <button @click="add">+1</button>
+    <button @click="emit">Emit Counter</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyCounter',
+  props: [
+    'value',
+  ],
+  data() {
+    return {
+      innerCounter: this.value,
+    };
+  },
+  methods: {
+    add() {
+      this.innerCounter += 1;
+    },
+    emit() {
+      this.$emit('input', this.innerCounter);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
